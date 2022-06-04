454e48625b81ca41135d77de27c882c5a40583b8
Chipskein <abfn0905@gmail.com>
2022-06-04 16:18:52 -0300

    nodemailer config

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 const express=require('express');
 const app=express();
+const dotenv=require('dotenv').config();
 const port=process.env.PORT || 8080;
-app.get('/',(req,res)=>{
+const { sendMail }=require('./conf/nodemailer');
+
+app.get('/',async (req,res)=>{
+    //await sendMail('abfn0905@gmail.com','teste','teste');
     return res.status(200).json('Running API');
 });
 app.listen(port,()=>console.log(`Started Server on ${port}`));
