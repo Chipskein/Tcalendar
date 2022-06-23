36db3bb918552b7c28beb197967f273c4aea1f38
Chipskein <abfn0905@gmail.com>
2022-06-23 16:51:51 -0300

    add models

---
+++ src/models/enterprises_inviteModel.js
@@ -0,0 +1,30 @@
+const { DataTypes, Model } = require('sequelize');
+const { db } = require('../conf/sequelize');
+class Enterprise_invite extends Model {}
+const tableConfig={ 
+    sequelize: db, 
+    schema: 'public',
+    modelName: 'Enterprise_invite'
+}
+const tableDefinition={
+    id_user:{
+        type:DataTypes.INTEGER,
+        references: {
+            model: Users,      
+            key: 'id',
+          }
+    },
+    id_enterprise:{
+        type:DataTypes.INTEGER,
+        references: {
+            model: Enterprise,      
+            key: 'id',
+          }
+    },
+    status:{
+        type:DataTypes.STRING
+    }
+}
+Enterprise_invite.init(tableDefinition,tableConfig);
+Enterprise_invite.sync();
+module.exports = { Enterprise_invite };
