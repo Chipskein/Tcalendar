c65d9edc03634573a247e5458b0403b398e9c932
Chipskein <abfn0905@gmail.com>
2022-06-23 20:51:03 -0300

    preparing image upload

---
+++ src/apis/imgur.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const FormData = require('form-data');
+const axios = require('axios').default;
+const clientId=process.env.IMGUR_CLIENT_ID;
+const imgurUploadURL='https://api.imgur.com/3/upload';
+module.exports={
+    UploadImage:async (file) =>{
+        const data = new FormData();
+        data.append('image',fs.createReadStream(file.path));
+        const config = {
+            method: 'post',
+            url: imgurUploadURL,
+            headers: { 
+                'Authorization': `Client-ID ${clientId}`,
+                "Content-Type": `multipart/form-data; boundary=${data._boundary}`,
+                ...data.getHeaders()
+            },
+            data:data
+        };
+        const response=await axios.request(config);
+        return response.data
+    },     
+}
--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,10 +1,12 @@
+const { UploadImage }=require('../apis/imgur')
 class UserController{
-    static register(req,res){
+    static async register(req,res){
         try{
-            
+            //const { link }=await UploadImage(req.file)
+            return res.status(200).json('Register User');
         }
         catch(err){
-
+            return res.status(500).json(err);
         }
     }
     static login(req,res){
