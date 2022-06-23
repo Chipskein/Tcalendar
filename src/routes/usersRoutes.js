fdb64699f8917f894e6ef3ae6fb789710cad7332
Chipskein <abfn0905@gmail.com>
2022-06-23 16:17:45 -0300

    preparando banco

--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -1,4 +1,5 @@
 const {Router} = require("express");
 const router=Router();
+const { Users } = require('../models/usersModel')
 router.get('/',(req,res)=>{return res.send('TESTANDo')});
 module.exports=router;
