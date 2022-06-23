4289707d7d5da7d428e42e22593abf3007fcf988
Chipskein <abfn0905@gmail.com>
2022-06-23 15:18:37 -0300

    add routes

--- src/index.js
+++ src/index.js
@@ -10,9 +10,23 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use(express.static('public'));
 
-app.get('/',async (req,res)=>{
-    return res.status(200).json('Running API');
-});
+//routes
+const enterprisesRoutes=require('./routes/enterprisesRoutes.js');
+const schedulesRoutes=require('./routes/schedulesRoutes.js');
+const timesRoutes=require('./routes/timesRoutes.js');
+const usersRoutes=require('./routes/usersRoutes.js');
+const enterprise_usersRoutes=require('./routes/enterprise_usersRoutes.js');
+const times_inviteRoutes=require('./routes/times_inviteRoutes.js');
+const times_usersRoutes=require('./routes/times_usersRoutes.js');
+
+app.use('/enterprises',enterprisesRoutes);
+app.use('/schedules',schedulesRoutes);
+app.use('/times',timesRoutes);
+app.use('/users',usersRoutes);
+app.use('/enterprise_users',enterprise_usersRoutes);
+app.use('/times_invite',times_inviteRoutes);
+app.use('/times_users',times_usersRoutes);
+
 
 app.listen(port,()=>console.log(`Started Server on ${port}`));
 
