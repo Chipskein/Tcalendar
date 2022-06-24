4d038067658d64128f1bc5a999e2e354b3dc5548
Chipskein <abfn0905@gmail.com>
2022-06-24 09:02:02 -0300

    create tempJTW

--- src/config/jsonwebtoken.js
+++ src/config/jsonwebtoken.js
@@ -1,9 +1,16 @@
 const jwt = require('jsonwebtoken');
 const Sign = process.env.JWT_SECRET;
 module.exports={
-    createJWTToken:(data)=>{
+    createSessionJWTToken:(data)=>{
         const JWT_CONFIG={
             expiresIn: '30d'
+        }
+        const token = jwt.sign(data, Sign, JWT_CONFIG);
+        return token;
+    },
+    createTempJWTToken:(data)=>{
+        const JWT_CONFIG={
+            expiresIn: '3h'
         }
         const token = jwt.sign(data, Sign, JWT_CONFIG);
         return token;
