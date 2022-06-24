19665f2143824c870a6e894a93f0ee51da7688d0
Chipskein <abfn0905@gmail.com>
2022-06-24 12:28:02 -0300

    userController quase pronto

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -3,6 +3,29 @@ const { hashPassword,verifyPassword } =require('../utils/password');
 const { Users }=require('../models/usersModel');
 const { createJWT,getDataFromToken }=require('../utils/jwt');
 class UserController{
+
+    static async showRegister(req,res){
+        return res.render('register');
+    }
+    static async showHome(req,res){
+        return res.render('home');
+    }
+    static async showLogin(req,res){
+        return res.render('login');
+    }
+    static async showResetPasswordForm(req,res){
+        try{
+            const { token }=req.query;
+            const { id } = getDataFromToken(token);
+            let user=await Users.findAll({where: { id }})
+            req.session.user=user;
+            return res.render('forgetPassword.ejs',{user});
+        }
+        catch(err){
+            return res.status(400).json(err.message);
+        }
+    }
+
     static async register(req,res){
         try{
             const {name,password,email} =req.body;
@@ -21,11 +44,11 @@ class UserController{
                     await user.save();
                 };
             };
-            const data=user.toJSON
-            return res.status(200).json(data);
+            const data=user.toJSON()
+            //send mail
+            return res.render('active_account',{user:data});
         }
         catch(err){
-            console.log(err);
             return res.status(500).json(err.message);
         }
     }
@@ -39,14 +62,13 @@ class UserController{
                 if(verifyPassword(password,user.password)){
                     delete(user.password);
                     req.session.user=user;
-                    //redirect to home
-                    return res.status(200).json(user);
+                    return res.redirect('/home');
                 } else{
-                    return res.status(401).json('Login inválido');
+                    return res.redirect('/login');
                 }
             } else{
-                //redirect to active account;
-                return res.status(200).json('Ative seu usuario');
+                //send mail
+                return res.render('active_account',{user:data});
             }
         }
         catch(err){
@@ -56,7 +78,7 @@ class UserController{
     static async logoff(req,res){
         try{
             req.session.user={};
-            return res.redirect('/');
+            return res.redirect('/login');
         }
         catch(err){
             return res.status(400).json(err.message);
@@ -64,18 +86,30 @@ class UserController{
     }
     static async update(req,res){
         try{
-
+            const updateDataRow={}
+            let {name,email,password} =req.body
+            const file=req.file;
+            if(password) updateDataRow.password=hashPassword(password);
+            if(name){
+                updateDataRow.name=name;
+                req.session.name=name;
+            };
+            if(email){
+                updateDataRow.email=email;
+                req.session.name=email;
+            };
+            if(file&&(file.mimetype=='image/jpeg'||file.mimetype=='image/gif'||file.mimetype=='image/png')){
+                const { link }=await UploadImage(req.file)
+                if(link){
+                    updateDataRow.img=link;
+                    req.session.img=link;
+                };
+            };
+            await Users.update(updateDataRow,{where: { id }});
+            return res.redirect('/home');
         }
         catch(err){
-
-        }
-    }
-    static async resetPassword(req,res){
-        try{
-
-        }
-        catch(err){
-
+            return res.status(400).json(err.message);
         }
     }
     static async activeUser(req,res){
@@ -84,8 +118,8 @@ class UserController{
             const { id } = getDataFromToken(token);
             await Users.update({ active: true },{where: { id }});
             let user=await Users.findAll({where: { id }})
-            user=user[0].dataValues
-            return res.status(200).json(user);
+            req.session.user=user;
+            return res.redirect('/home');
         }
         catch(err){
             return res.status(400).json(err.message);
--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -3,11 +3,20 @@ const router=Router();
 const { upload }=require('../config/multer');
 const UserController = require("../controllers/usersController");
 
-router.post('/',upload.single('image'),UserController.register);
+//not logged
+router.get('/login',UserController.showLogin);
+router.get('/register',UserController.showRegister);
+
+router.post('/register',upload.single('image'),UserController.register);
 router.post('/login',UserController.login);
-router.get('/logoff',UserController.logoff);
-router.patch('/',UserController.update);
-router.get('/reset',UserController.resetPassword);
+
+router.get('/reset',UserController.showResetPasswordForm);
 router.get('/active',UserController.activeUser);
+
+//logged
+router.get('/home',UserController.showHome);
+router.get('/logoff',UserController.logoff);
+router.post('/update',upload.single('image'),UserController.update);
+
 
 module.exports=router;
