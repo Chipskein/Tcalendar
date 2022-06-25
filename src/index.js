e09c86e4954a9da4e875f4e8871043e589f11330
victor-grandmasterwitcher <victortavamaral@gmail.com>
2022-06-24 22:17:04 -0300

    login e cadastro ok

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config()
 const express=require('express');
 const session=require('express-session');
 const sessionConfig=require('./config/express-session')
