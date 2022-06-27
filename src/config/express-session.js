6595efec5c54a88bb224de27e0d4a1f2005d42d6
Chipskein <abfn0905@gmail.com>
2022-06-27 20:39:51 -0300

    rk express-session

--- src/config/express-session.js
+++
@@ -1,7 +0,0 @@
-const sessionSecret=process.env.EXPRESS_SESSION_SECRET;
-module.exports={
-    secret: sessionSecret,
-    resave: false,
-    saveUninitialized: false,
-    cookie: { secure: false }
-}
