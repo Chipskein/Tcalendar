716868444734b77f9c5112ea634e8d67c4375dfd
Chipskein <abfn0905@gmail.com>
2022-06-28 20:41:00 -0300

    removing useless files

--- src/routes/enterprise_usersRoutes.js
+++
@@ -1,6 +0,0 @@
-const {Router} = require("express");
-const controller = require("../controllers/enterprises_usersController");
-const { isLogged ,isInEnterprise,isOwner}=require('../utils/middlewares');
-const router=Router();
-router.post('/',isLogged,isInEnterprise,controller.addUser);
-module.exports=router;
